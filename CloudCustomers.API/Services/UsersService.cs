082ca7a894e295a3c15546fbed814289ba2cd638
Raymond van Hoorn <vanhoornraymond@gmail.com>
2022-06-27 13:23:08 +0200

    Add project files.

---
+++ CloudCustomers.API/Services/UsersService.cs
@@ -0,0 +1,16 @@
+﻿using CloudCustomers.API.Models;
+using System;
+
+public interface IUsersService
+{
+	public Task<List<User>> GetAllUsers();
+}
+
+public class UsersService : IUsersService
+{
+    public Task<List<User>> GetAllUsers()
+    {
+        throw new NotImplementedException();
+    }
+}
+
